a9d15712742a851d18cf842d256b674cfc9f7ea1
Everdeana <87472190+Everdeana@users.noreply.github.com>
2021-11-02 11:40:44 +0900

    Create PropertyServlet1.java

---
+++ PropertyServlet1.java
@@ -0,0 +1,21 @@
+@WebServlet("/PropertyServlet1")
+public class ProperteyServlet1 extends HTTPServlet {
+    private static final long serialVersionUID = 1L;
+    
+    /**
+     * @see HttpServlet#HTTPServlet()
+     */
+    public PropertyServlet1() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    
+    /**
+     * @see HttpServlet#doGet(HTTPServletRequest request
+     */
+    protected void doGet(HTTPServletRequest request, HtpServletResponse response) {
+        // TODO Auto-generated method stub
+        // response.getWriter().append("Served at: ").append
+    
+
+  
