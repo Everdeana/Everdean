e2737f298286cc664a2ff47ca10b8ccb36bd797e
Everdeana <87472190+Everdeana@users.noreply.github.com>
2021-11-11 09:38:10 +0900

    Create app_led.py

---
+++ app_led.py
@@ -0,0 +1,28 @@
+form flask import Flask
+import RPi.GPIO as GPIO
+
+app = Flask(__name__)
+
+LED = 8
+GPIO.setmode(GPIO.BOARD) # BOARD는 커넥터 pin번호 사용
+GPIO.setup(LED, GPIO.OUT, initial=GPIO.LOW)
+
+@app.route("/")
+def helloworld():
+	return "Hello World"
+
+@app.route("/led/on")
+def led_on():
+	return "LED ON"
+
+@app.route("/led/off")
+def led_off():
+	return "LED OFF"
+  
+@app.route("/gpio/cleanup")
+def gpio_cleanup():
+    GPIO.cleanup()
+    return "GPIO CLEANUP"
+
+if __name__ == "__main__":
+	app.run(host="0.0.0.0")
