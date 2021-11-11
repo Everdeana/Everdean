e26707437075bf1a9806193b4939be99b5e91f0a
Everdeana <87472190+Everdeana@users.noreply.github.com>
2021-11-11 09:46:31 +0900

    Update app_start.py

--- app_start.py
+++ app_start.py
@@ -1,18 +1,18 @@
-form flask import Flask
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def helloworld():
-	return "Hello World"
+    return "Hello World"
 
-@app.route("/led/on")
-def led_on():
-	return "LED ON"
+#@app.route("/led/on")
+#def led_on():
+#    return "LED ON"
 
-@app.route("/led/off")
-def led_off():
-	return "LED OFF"
+#@app.route("/led/off")
+#def led_off():
+#    return "LED OFF"
 
 if __name__ == "__main__":
-	app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0")
