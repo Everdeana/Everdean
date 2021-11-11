ad0fd76b31413b55d65c33a552fee97fbe83e690
Everdeana <87472190+Everdeana@users.noreply.github.com>
2021-11-11 09:51:39 +0900

    Create cron.py

---
+++ cron.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.cleanup()
+
+LED = 8
+GPIO.setmode(GPIPO.BOARD)
+GPIO.setup(LED, GPIO.OUT, initial=GPIO.LOW)
+
+try:
+    num = 0
+    while(True)
+        time.sleep(1)
+        GPIO.output(LED, GPIO.HIGH)
+        time.sleep(1)
+        GPIO.output(LED, GPIO.LOW)
+        num = num + 1
+        if(num == 5):
+            GPIO.cleanup()
+            break
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
