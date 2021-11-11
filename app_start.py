025b80254c54fb3f2ea126a0366e66e69c7771a6
Everdeana <87472190+Everdeana@users.noreply.github.com>
2021-11-11 09:33:42 +0900

    Create app_start.py

---
+++ app_start.py
@@ -0,0 +1,18 @@
+form flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def helloworld():
+	return "Hello World"
+
+@app.route("/led/on")
+def led_on():
+	return "LED ON"
+
+@app.route("/led/off")
+def led_off():
+	return "LED OFF"
+
+if __name__ == "__main__":
+	app.run(host="0.0.0.0")
