bfde11495ccbe027deb7fb6c7bfa8f3377d80e6b
Everdeana <87472190+Everdeana@users.noreply.github.com>
2022-03-29 16:50:04 +0900

    Update ArithmeticEx01.java

--- Asgmt/Asgmt04/ArithmeticEx01.java
+++ Asgmt/Asgmt04/ArithmeticEx01.java
@@ -2,13 +2,13 @@ package Asgmt04;
 
 public class ArithmeticEx01 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { // 클래스를 실행하기 위한 Main Method
 		// TODO Auto-generated method stub
-		int v1 = 5;
+		int v1 = 5; // 계산에 사용할 정수형 변수 선언
 		int v2 = 2;
 		
-		int result1 = v1 + v2;
-		System.out.println("result1= " + result1);
+		int result1 = v1 + v2; // +연산
+		System.out.println("result1= " + result1); // 계산한 값 출력
 		
 		int result2 = v1 - v2;
 		System.out.println("result2= " + result2);
