8fa748f3d3d7e5843f6f41545a9a58955a30569d
Everdeana <87472190+Everdeana@users.noreply.github.com>
2022-08-30 11:39:12 +0900

    Create 08_30_test01.py

---
+++ 08_30_test01.py
@@ -0,0 +1,11 @@
+print("Python Test")
+a = 10
+b = 90
+result = a + b
+print(a, "+", b, "=", result)
+result = a - b
+print(a, "-", b, "=", result)
+result = a * b
+print(a, "*", b, "=", result)
+result = a / b
+print(a, "/", b, "=", result)
