c4c4f73d73883f4b7e8dedfa533cc4c04e7953b2
Everdeana <87472190+Everdeana@users.noreply.github.com>
2022-09-07 09:40:48 +0900

    Create Test01.java

---
+++ Week01/Test01.java
@@ -0,0 +1,14 @@
+package ch01Test;
+
+public class Test01 {
+
+	public static void main(String[] args) { // String은 자료형이 아닌 class type --> args 변수로 받음
+		// TODO Auto-generated method stub
+		System.out.println("Java Run Test");
+		int a = 10;
+		int b = 10;
+		int c = a + b;
+		System.out.println("결과는" + c + "입니다.");
+	}
+
+}
