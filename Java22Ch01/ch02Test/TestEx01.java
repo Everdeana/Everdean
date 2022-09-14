a0b89c7cadc12b4dc2c53cd0c13e2cd0f139121c
Everdeana <87472190+Everdeana@users.noreply.github.com>
2022-09-14 10:08:08 +0900

    Create TestEx01.java

---
+++ Java22Ch01/ch02Test/TestEx01.java
@@ -0,0 +1,14 @@
+package ch02Test;
+
+public class TestEx01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 3;
+		double c = (double) a / (double) b;
+		System.out.println(c);
+
+	}
+
+}
