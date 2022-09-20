684798170fd52f42bc2e0f0614184d2cb0558e49
Everdeana <87472190+Everdeana@users.noreply.github.com>
2022-09-20 12:28:59 +0900

    Update 09_20_test02.py

--- Week04/09_20_test02.py
+++ Week04/09_20_test02.py
@@ -1,11 +1,11 @@
 aa = [0, 0, 0, 0] # list type
 sum = 0
 
-aa[0] = int(input("1번째 정수:"))
-aa[1] = int(input("2번째 정수:"))
-aa[2] = int(input("3번째 정수:"))
-aa[3] = int(input("4번째 정수:"))
+aa[0] = int(input("Enter first integer:")) # Convert data type to integer
+aa[1] = int(input("Enter second integer:"))
+aa[2] = int(input("Enter third integer:"))
+aa[3] = int(input("Enter fourth integer:"))
 
 sum = aa[0] + aa[1] + aa[2] + aa[3]
 
-print("결과=> %d" %sum)
+print("result=> %d" %sum)
