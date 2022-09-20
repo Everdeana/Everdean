0993a9792ee31b9a07a5c2be140581b15bde632b
Everdeana <87472190+Everdeana@users.noreply.github.com>
2022-09-20 12:42:52 +0900

    Create 09_20_test05.py

---
+++ Week04/09_20_test05.py
@@ -0,0 +1,17 @@
+# Two-Dimensional
+
+list1 = []
+list2 = []
+
+value = 1
+for i in range(0, 3):
+    for k in range(0, 4):
+        list1.append(value)
+        value = value + 1
+    list2.append(list1)
+    list1 = []
+    
+for i in range(0, 3):
+    for k in range(0, 4):
+        print("%3d" %list2[i][k], end="")
+    print("")
