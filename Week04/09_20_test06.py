c67af3cc354c9f1271dafed68b1474c52c738754
Everdeana <87472190+Everdeana@users.noreply.github.com>
2022-09-20 12:57:10 +0900

    Create 09_20_test06.py

---
+++ Week04/09_20_test06.py
@@ -0,0 +1,16 @@
+import imp
+from platform import win32_edition
+from tkinter import *
+
+window = Tk() # Object Variables provided by tkinter
+print(type(window))
+window.title("Window")
+window.geometry("400x100")
+'''
+(variable) geometry: Overload[(newGeometry: None = ...)
+    -> str, (newGeometry: str) -> None]
+Set geometry to NEWGEOMETRY of the form =widthxheight+x+y. 
+Return current value if None is given.
+'''
+window.resizable(width = False, height = False) # Window size can't be changed
+window.mainloop()
