a06edca1f73262b10251fec92167e190ae7223a5
Everdeana <87472190+Everdeana@users.noreply.github.com>
2022-09-20 13:12:34 +0900

    Create 09_20_test07.py

---
+++ Week04/09_20_test07.py
@@ -0,0 +1,14 @@
+from cProfile import label
+from tkinter import *
+
+window = Tk()
+
+label1 = Label(window, text = "SmartIT, 데이터 분석 과정")
+label2 = Label(window, text = "Study Hard", font = ("맑은고딕", 30), fg = "blue")
+label3 = Label(window, text = "More Hard", bg = "magenta", width = 20, height  = 5, anchor = SE)
+
+label1.pack()
+label2.pack()
+label3.pack()
+
+window.mainloop()
